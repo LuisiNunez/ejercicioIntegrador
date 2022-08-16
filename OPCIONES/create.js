0051ab90ea5fc9ae904c70253847470f6a84a6ca
Juan Roberts <juancroberts31@gmail.com>
2022-08-16 18:08:22 -0300

    Correcion TP Integrador
    No se genera ID correctamente

--- OPCIONES/create.js
+++ OPCIONES/create.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// El ID debe generarlo tu algoritmo y no es un valor que se recibe por comando cuando un usuario busca añadir un libro nuevo. El desafío es poder pensar
+// una manera en que tu código genere IDs únicos y no repetidos por sí mismo.
 function create(id, title, author, genre, year, cost, price) {
   let data = fs.readFileSync("./baseDeDatos.json", "utf-8");
   let dataParseada = JSON.parse(data);
